7e99d0b9e8d9780d49ca6571fdf260517a92581e
sara <saraabdelaziz461@gmail.com>
2026-03-12 21:47:58 +0200

    final commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,16 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { resolve } from "node:path";
-import { UsersModule } from "../modules/users/users.module";
-import { AuthModule } from "../modules/auth/auth.module";
+import { UsersModule } from "./modules/users/users.module";
+import { AuthModule } from "./modules/auth/auth.module";
 import { MongooseModule } from "@nestjs/mongoose";
 import { logger } from "./middleware/logger.middleware";
-import { BrandModule } from "../modules/brand/brand.module";
-import { CategoryModule } from "../modules/category/category.module";
-import { ProductModule } from "../modules/product/product.module";
-import { CartModule } from "../modules/cart/cart.module";
-import { CouponModule } from '../modules/coupon/coupon.module';
-import { OrderModule } from '../modules/order/order.module';
+import { BrandModule } from "./modules/brand/brand.module";
+import { CategoryModule } from "./modules/category/category.module";
+import { ProductModule } from "./modules/product/product.module";
+import { CartModule } from "./modules/cart/cart.module";
+import { CouponModule } from './modules/coupon/coupon.module';
+import { OrderModule } from './modules/order/order.module';
 
 @Module({
   imports: [
